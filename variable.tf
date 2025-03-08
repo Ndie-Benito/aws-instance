8c9264a4e124804ef27d5a9e4da79d1145535e4a
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-03-07 19:50:13 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -9,3 +9,15 @@ variable "instance_name" {
   type        = string
   default     = "HelloWorld"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
